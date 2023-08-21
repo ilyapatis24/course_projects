86eba746aa78d7ae540bfdfb6e6032febcf6aec0
ilyapatis24 <ilyapatis24@mail.ru>
2023-08-21 21:15:18 +0300

    relocating folders

--- course_work_backup/main.py
+++
@@ -1,9 +0,0 @@
-import vk, ya_disk
-
-
-if __name__ == '__main__':
-    user1 = vk.VK(user_id='', token='')
-    user1.get_photos(user_id='')
-    user1 = ya_disk.YandexDisk(token='')
-    user1.create_folder(folder='')
-    user1.upload_file(folder='')
